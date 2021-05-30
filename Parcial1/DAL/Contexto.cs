12bf6182baa75fb7a769af82faa5f6e05b72367f
Anderson Rodriguez Molina <anderson_rodriguez@ucne.edu.do>
2021-05-30 00:16:07 -0400

    Add project files.

---
+++ Parcial1/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+//using Parcial1.Models; 
+
+ 
+
+namespace Parcial1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public int MyProperty { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\Parcial1.db");
+
+        }
+    }
+}
