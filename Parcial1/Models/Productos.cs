f91b4aac751a7dc2afb5ba1f2a70cf72bb7a0ccf
Anderson Rodriguez Molina <anderson_rodriguez@ucne.edu.do>
2021-06-02 18:28:29 -0400

    Se creo la clase Productos

---
+++ Parcial1/Models/Productos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1.Models
+{
+    public class Productos
+    {
+        [Key]
+
+        public int ArticuloId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Costo { get; set; }
+        public int Inventario { get; set; }
+
+    }
+}
